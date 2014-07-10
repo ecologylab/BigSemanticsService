2121d1ab8fc474612fe1fbe28b25201784782014
ecologylab <ecologylab@semantic-service.(none)>
2014-07-09 19:17:43 -0500

    Fixed bug in forker.py

--- Scripts/main/forker.py
+++ Scripts/main/forker.py
@@ -7,11 +7,13 @@ from subprocess import Popen, PIPE, STDOUT
 DEVNULL = open(os.devnull, "w")
 
 def fork(cmds, wd = None):
-  print wd + " (fork) $ " + " ".join(cmds)
+  wd_name = wd if wd is not None else os.getcwd()
+  print wd_name + " (fork) $ " + " ".join(cmds)
   p = Popen(cmds, stderr=STDOUT, stdout=DEVNULL, cwd = wd)
 
 def call(cmds, wd = None):
-  print wd + " (call) $ " + " ".join(cmds)
+  wd_name = wd if wd is not None else os.getcwd()
+  print wd_name + " (call) $ " + " ".join(cmds)
   p = Popen(cmds, stdout=PIPE, stderr=PIPE, cwd = wd)
   (out, err) = p.communicate()
   return (p.returncode, out, err)
@@ -19,7 +21,8 @@ def call(cmds, wd = None):
 def check_call(cmds, wd = None):
   (code, out, err) = call(cmds, wd)
   if code != 0:
+    wd_name = wd if wd is not None else os.getcwd()
     raise exceptions.RuntimeError(
             "Failed to execute {}\n  in {}\n  OUT:\n{}\n  ERROR:\n{}\n".format(
-              cmds, wd, out, err))
+              cmds, wd_name, out, err))
 
