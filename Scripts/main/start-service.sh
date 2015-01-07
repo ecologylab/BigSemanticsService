b7de9891593f734d1ac144eea18d36f411aca10e
Yin Qu <me.yin.qu@gmail.com>
2015-01-07 17:30:09 -0600

    Added resource filters to eclipse .project files. Modified start-service.sh to use the new structure.

--- Scripts/main/start-service.sh
+++ Scripts/main/start-service.sh
@@ -6,23 +6,29 @@ _BSS_DIR=${BSS_DIR:="$HOME/bigsemantics-service"}
 echo "BigSemantics Service Dir: $_BSS_DIR"
 cd "$_BSS_DIR"
 
-echo "Stopping running service if any ..."
 if [ -d "$_BSS_DIR" ]; then
+  if [ -d "$_BSS_DIR/bs-code" ]; then
+    echo "Updating BigSemantics code ..."
+    cd $_BSS_DIR/bs-code
+    git submodule foreach git checkout -- "*"
+    git submodule foreach git clean -f
+    git submodule foreach git clean -f -d
+    git checkout -- "*"
+    git clean -f
+    git clean -f -d
+    git pull
+  fi
+
+  echo "Stopping running service if any ..."
   killall -9 java
 
-  cd $_BSS_DIR/jetty-dist
-  # By default, jetty runs on port 8080
+  cd $_BSS_DIR/deploy
+  # By default, the service runs on port 8080
   # You may want to map port 8080 to 80 in your system, e.g.:
   #   sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
   echo "Starting BigSemantics service ..."
-  nohup java -server -jar start.jar 2>&1 > /dev/null &
-
-  sleep 60
-
-  echo "Starting downloader ..."
-  cd $_BSS_DIR/downloader
-  nohup java -server -Xms128m -Xmx256m -jar Downloader.jar 2>&1 >/dev/null &
-
+  nohup java -server -jar BigSemanticsService.jar 2>&1 > /dev/null &
+  sleep 5
   echo "Done."
 else
   echo "Service directory not found: $_BSS_DIR"
